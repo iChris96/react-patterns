e61f7b9e7bd28227567f23540c45e286922846f8
christopher_windows <christopher_x10x@hotmail.com>
2021-05-18 20:47:21 -0500

    using a custom hook to fetch pokemons api

--- src/custom-hook/components/custom-hook-page.js
+++ src/custom-hook/components/custom-hook-page.js
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const CustomHookPage = () => (
-  <>
-    <h2>Custom Hook</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+const endpoint = "pokemon?limit=100"
+const url = `https://pokeapi.co/api/v2/${endpoint}/`
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log("we have data!", response);
+        setData(response)
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [])
+
+
+  return {
+    data,
+    isLoading,
+    isError
+  }
+}
+
+export const CustomHookPage = () => {
+
+  const { data, isLoading, isError } = useFetch(url);
+
+  if (isLoading) {
+    return 'Loading...';
+  }
+
+  if (isError) {
+    return <p>{isError.message}</p>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const pokemonList = data.results.map((pokemon) =>
+    <li>{pokemon.name}</li>
+  );
+
+
+
+  return (
+    <>
+      <h2>Pokemons List - Using Custom Hooks </h2>
+
+      <ul>{pokemonList}</ul>
+
+    </>
+  )
+}
