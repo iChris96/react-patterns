684c92d69c3846049d2a19829d4187069e5596e4
christopher_windows <christopher_x10x@hotmail.com>
2021-05-18 21:12:57 -0500

    implementing withFetch as HOC

--- src/hoc/components/hoc-page.js
+++ src/hoc/components/hoc-page.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const HocPage = () => (
-  <>
-    <h2>High Order Component (HOC)</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+
+const withFetch = (FORM, url) => {
+
+  const ControlledFetch = () => {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+    const [isError, setError] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const data = await fetch(url);
+          const response = await data.json();
+          console.log("we have data!", response);
+          setData(response)
+        } catch (e) {
+          setError(e);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      fetchData();
+    }, [])
+
+
+    if (isLoading) {
+      return 'Loading...';
+    }
+
+    if (isError) {
+      return <p>{isError.message}</p>;
+    }
+
+    if (!data) {
+      return null;
+    }
+
+    return (<FORM data={data} />)
+
+  }
+
+  return ControlledFetch;
+}
+
+const endpoint = "pokemon?limit=100"
+const url = `https://pokeapi.co/api/v2/${endpoint}/`
+
+export const HocPage = withFetch(({ data }) => {
+
+  const pokemonList = data.results.map((pokemon) =>
+    <li>{pokemon.name}</li>
+  );
+  return (
+    <>
+      {pokemonList}
+    </>
+  )
+
+}, url)
+
