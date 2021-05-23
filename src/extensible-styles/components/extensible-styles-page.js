f80db543bd28570cf26f8e550cb98717b81f527c
christopher_windows <christopher_x10x@hotmail.com>
2021-05-23 18:13:18 -0500

    handling extensible styles

--- src/extensible-styles/components/extensible-styles-page.js
+++ src/extensible-styles/components/extensible-styles-page.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.css'
 
-export const ExtensibleStylesPage = () => (
-  <>
-    <h2>Extensible Styles</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+
+const CustomButton = ({
+  name, type = 'button', onClick, className = 'btn', style = {}, children
+}) => {
+
+  return (
+    <button
+      name={name}
+      type={type}
+      onClick={onClick}
+      style={style}
+      className={className}
+
+    >{children}</button>
+  )
+}
+
+CustomButton.propTypes = {
+  type: PropTypes.oneOf(['button', 'submit']).isRequired,
+  children: PropTypes.node.isRequired,
+  name: PropTypes.string,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export const ExtensibleStylesPage = () => {
+
+  const onClick = () => {
+    console.log('clicked')
+  }
+
+  return (
+    <>
+      <h2>Extensible Styles</h2>
+      <CustomButton
+        type='button'
+        onClick={onClick}
+        name='btn'
+        className='btn coral-btn'
+      >Click me!</CustomButton>
+
+      <br />
+      <CustomButton
+        type='button'
+        onClick={onClick}
+        name='btn'
+        className='btn red-btn'
+        style={{ color: 'white', backgroundColor: 'blue' }}
+      >Click me!</CustomButton>
+
+      <br />
+      <CustomButton
+        type='button'
+        onClick={onClick}
+        name='btn'
+      >Click me!</CustomButton>
+    </>
+  )
+};
