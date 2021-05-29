14dec7cf084be35302ecbe89573007b73c4a97f6
christopher_windows <christopher_x10x@hotmail.com>
2021-05-29 15:59:32 -0500

    example unsing render props!

--- src/render-props/components/render-props-page.js
+++ src/render-props/components/render-props-page.js
@@ -1,8 +1,61 @@
 import React from 'react';
+import { useFetch } from '../../custom-hook/components/custom-hook-page';
 
-export const RenderPropsPage = () => (
-  <>
-    <h2>Render Props</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+
+const endpoint = "pokemon?limit=10"
+const url = `https://pokeapi.co/api/v2/${endpoint}/`
+
+const List = ({ data, isLoading }) => {
+
+  if (isLoading) return (<div>is Loading......</div>)
+
+  if (!data) return null
+
+  return (
+    <div>
+      {
+        data.results.map(e => (<h3>{e.name}</h3>))
+      }
+    </div>
+  )
+}
+
+
+const ListWithRenderProps = ({ data, isLoading, renderItem, renderLoading }) => {
+
+  if (isLoading) return (renderLoading())
+
+  if (!data) return null
+
+  return (
+    <div>
+      {
+        data.results.map(e => (renderItem(e)))
+      }
+    </div>
+  )
+
+}
+
+export const RenderPropsPage = () => {
+
+  const { data, isLoading, isError } = useFetch(url);
+
+  console.log(data)
+
+  return (
+    <>
+      <h2>No - Render Props</h2>
+      <List data={data} isLoading={isLoading} />
+      <hr />
+      <h2>With - Render Props</h2>
+      <ListWithRenderProps
+        data={data}
+        isLoading={isLoading}
+        renderLoading={() => <div>is Loading...... ..</div>}
+        renderItem={(item) => <h3 style={{ color: 'blue' }}>{item.name}</h3>}
+
+      />
+    </>
+  );
+}
