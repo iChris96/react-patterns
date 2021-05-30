f2464ca9e6a61ee2a485e52018772316962e7e1b
christopher_windows <christopher_x10x@hotmail.com>
2021-05-30 14:26:22 -0500

    using controlled props

--- src/control-props/components/control-props-page.js
+++ src/control-props/components/control-props-page.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const ControlPropsPage = () => (
-  <>
-    <h2>Control Props</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+
+const MyInput = () => {
+  const [val, setVal] = useState('') //we cannot access to the state from outside
+
+  const handleOnChange = ({ target: { value } }) => {
+    setVal(value)
+  }
+
+  return (
+    <input value={val} onChange={handleOnChange} />
+  )
+}
+
+
+const MyControlledInput = ({ value, setValue }) => {
+
+  const handleOnChange = ({ target: { value } }) => {
+    setValue(value)
+  }
+
+  return (
+    <input value={value} onChange={handleOnChange} />
+  )
+}
+
+
+export const ControlPropsPage = () => {
+
+  const [val, setVal] = useState('') //now we can handle the state from outside
+
+  return (
+    <>
+      <h2>No - Control Props</h2>
+      <MyInput />
+      <button onClick={() => { console.log('nothing to do..') }}>clean input</button>
+      <hr />
+      <MyControlledInput value={val} setValue={(value) => { setVal(value) }} />
+      <button onClick={() => { setVal('') }}>clean input</button>
+    </>
+  )
+};
